5f15992c399ad957cab4e0202d77e0a9b049f069
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2023-03-21 20:02:40 -0400

    criacao da funcao cactus

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 const dino = document.querySelector(".dino");
+const background = document.querySelector(".background");
 let isJumping = false;
 function handleKeyUp(event) {
     // apertou botao pra pular
     if (event.keyCode === 32) {
         if (!isJumping) {
-            jump();  
+            jump();
         }
     }
 }
@@ -19,7 +20,7 @@ function jump() {
                 //pra evitar que desca indefinidamente
                 if (position <= 0) {
                     clearInterval(downInterval);
-                    isJumping = false
+                    isJumping = false;
                 } else {
                     //descer
                     position -= 20;
@@ -33,5 +34,25 @@ function jump() {
         }
     }, 20);
 }
+function createCactus() {
+    const cactus = document.createElement("div");
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+    cactus.classList.add("cactus");
+    cactus.style.left = 1000 + "px";
+    background.appendChild(cactus);
 
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + "px";
+        }
+    }, 20);
+    setTimeout(createCactus, randomTime);
+
+}
+createCactus();
 document.addEventListener("keyup", handleKeyUp);
